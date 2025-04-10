e9ba284f7e7a355d9df9312f18487a39ee69fb79
Skrembl <akimowiljagg@outlook.com>
2025-04-10 12:17:34 +0300

    Сохранение изменений

---
+++ app.js
@@ -0,0 +1,10 @@
+const burgerMenu = document.getElementsByClassName("burger-menu");
+const closeMenu = document.getElementsByClassName("close-menu");
+const visibleMenu = document.getElementsByClassName("adaptive");
+
+dispatchEvent.addEventListener('click', () => {
+    // visibleMenu.
+    // if (visibleMenu) {
+
+    // }
+})
