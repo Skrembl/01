e3faba04b9bec34276d44ffbb2cb9e320f01424a
Skrembl <akimowiljagg@outlook.com>
2025-04-11 08:29:55 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const burgerMenu = document.getElementsByClassName("burger-menu");
-const closeMenu = document.getElementsByClassName("close-menu");
-const visibleMenu = document.getElementsByClassName("adaptive");
+const burger = document.querySelector(".burger-menu");
+const adaptive = document.querySelector(".adaptive");
 
-dispatchEvent.addEventListener('click', () => {
-    // visibleMenu.
-    // if (visibleMenu) {
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    adaptive.classList.toggle('active');
 
+    // if (adaptive.classList.contains('active')) {
+    //     document.body.style.overflow = 'hidden';
+    // } else {
+    //     document.body.style.overflow = 'visible';
     // }
-})
+});
+
+document.addEventListener('click', (e) => {
+    if (!e.target.closest('.adaptive') && !e.target.closest('.burger-menu')) {
+        burger.classList.remove('active');
+        adaptive.classList.remove('active');
+        document.body.style.overflow = 'visible';
+    }
+});
+
+document.querySelectorAll('.close-menu').forEach(link => {
+    link.addEventListener('click', () => {
+        burger.classList.remove('active');
+        adaptive.classList.remove('active');
+        document.body.style.overflow = 'visible';
+    });
+});
